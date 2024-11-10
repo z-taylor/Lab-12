66d1a464f67699824ad885fd619e2d6e5b3df264
Zachary Taylor <120347442+z-taylor@users.noreply.github.com>
2024-11-10 17:40:37 -0500

    commit

--- Lab12A.py
+++ Lab12A.py
@@ -14,5 +14,5 @@ legs = int(input("You are about to create a chair.\nHow many legs does your chai
 rolling = True if input("Is your chair rolling (true/false): ").lower() in ("true", "t") else False
 material = input("What is your chair made of: ")
 chair1 = chair(legs, rolling, material)
-print(f"Your chair has {chair1.numOfLegs} legs, {"is" if chair1.rolling==True else "is not"} rolling, and is made of {chair1.material}.\nThis program is going to change that.\nYour chair has 4 legs, is not rolling, and is made of wood.")
+print(f"Your chair has {chair1.numOfLegs} legs, {'is' if chair1.rolling==True else 'is not'} rolling, and is made of {chair1.material}.\nThis program is going to change that.\nYour chair has 4 legs, is not rolling, and is made of wood.")
 chair1.numOfLegs, chair1.rolling, chair1.material = 4, False, "wood"
